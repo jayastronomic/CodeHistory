f81be8af89db0f43847258fe6bb7aab5b5f377d1
jayastronomic <61813086+jayastronomic@users.noreply.github.com>
2022-11-28 15:32:56 -0600

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Julian Smith on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
